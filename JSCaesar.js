7c84d5c749a61a7d452b3f8bb86ac03f8ada3078
Jacob C Moore <jmooree30@gmail.com>
2019-02-12 02:50:35 -0600

    arrow function/ es6 variables (#1)

--- JSCaesar.js
+++ JSCaesar.js
@@ -1,20 +1,17 @@
-let caesar = function(string,shift) {
-  
-  let cipheredString = '';
+let caesar = (string, shift) => {
+  let cipheredString = "";
 
-  for (let i = 0; i < string.length; i++){
-    var char = string[i];
-    
-    var code = string[i].charCodeAt();
-     if (code >= 97 && code <= 122){
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i];
+    let code = string[i].charCodeAt();
+    if (code >= 97 && code <= 122) {
       char = String.fromCharCode(((code - 97 + shift) % 26) + 97);
-      
     } else if (code >= 65 && code <= 90) {
       char = String.fromCharCode(((code - 65 + shift) % 26) + 65);
     }
-    
     cipheredString += char;
   }
-  
   return cipheredString;
 };
+
+caesar("string", 2);
